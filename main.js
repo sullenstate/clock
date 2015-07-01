b856783d9440d368d9109fd0d6c51b5cd999e3b0
Tim <tklein@outlook.com>
2015-06-30 19:29:38 -0600

    Final Wednesday Commit

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 $(document).on('ready', function() {
 
+	setInterval(function(){
+		var now = new Date();
+		$('.text').text(now.toLocaleTimeString());
+		if (now.getHours()>=12) {
+			$('.indicator').css('top', '23px');
+		}
+	}, 1000);
+
+
+
 	var outerShell = $('<div class="outer-shell"></div>');
 	var innerShell = $('<div class="inner-shell"></div>');
-	var labels = $('<div class="labels"><ul><li>AM</li><li>PM</li><li>Auto</li></ul></div>');
+	var labels = $('<div class="labels"><ul><li>AM</li><li class="PM">PM</li><li>Auto</li></ul></div>');
 	var clockScreen = $('<div class="screen"></div>');
 	var indicator = $('<div class="indicator">◉</div>');
-	var text = $('<div class="text">12:45</div>');
+	var text = $('<div class="text"></div>');
 	var am = $('<div class="am-freq"><ul><li>AM</li><li>530</li><li>550</li><li>650</li><li>800</li><li>1000</li><li>1300</li><li>1600</li><li>KHz</li></ul></div>');
 	var fm = $('<div class="fm-freq"><ul><li>FM</li><li>88</li><li>92</li><li>96</li><li>100</li><li>104</li><li>108</li><li>MHz</li></ul></div>');
 
